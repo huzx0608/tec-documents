d504e073198534bf4c591325dab1146860123580
Zongxing Hu <huzongxing0826@gmail.com>
2021-09-27 17:43:04 +0800

    [doc] update index and source files

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,8 @@ exclude_patterns = []
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
+source_suffix = ['.rst', '.md']
+master_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
